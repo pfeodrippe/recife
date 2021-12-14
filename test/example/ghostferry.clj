1f2579a9e59889a103f5174107e8f47f86eaad25
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2021-12-13 22:13:32 -0300

    ghostferry

---
+++ test/example/ghostferry.clj
@@ -0,0 +1,7 @@
+(ns example.ghostferry
+  "See https://shopify.engineering/mysql-database-shard-balancing-terabyte-scale and
+  https://github.com/Shopify/ghostferry/tree/master/tlaplus."
+  (:require
+   [recife.core :as r]))
+
+#_(r/defproc)
