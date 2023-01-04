be59d3ef45da88fba9dc90dfc293931a67d10ee9
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2023-01-04 01:10:05 -0500

    add charts

--- test/example/knuth_yao_die.clj
+++ test/example/knuth_yao_die.clj
@@ -2,10 +2,44 @@
   "Based on https://github.com/tlaplus/Examples/blob/master/specifications/KnuthYao/KnuthYao.tla.
 
   Also see https://www.youtube.com/watch?v=cYenTPD7740."
+  {:nextjournal.clerk/visibility {:code :hide :result :hide}
+   :nextjournal.clerk/no-cache true}
   (:require
    [recife.core :as r]
    [recife.helpers :as rh]
-   [recife.communication :as r.buf]))
+   [recife.communication :as r.buf]
+   [com.pfeodrippe.tooling.clerk :as tool.clerk]
+   [nextjournal.clerk :as clerk]
+   [nextjournal.clerk.viewer :as viewer]))
+
+;; ◊page-name[{:subtitle "6-sided die"}]{knuth yao}
+
+(add-watch r.buf/*contents ::contents
+           (fn [_k _r _old new]
+             (when (zero? (mod (count new) 1000))
+               (clerk/recompute!))))
+
+^{::clerk/visibility {:result :show}}
+(clerk/plotly {:data [{:values (let [{:keys [h t]
+                                      :or {h 1 t 0}}
+                                     (frequencies (mapv :face (r.buf/read-contents)))]
+                                 [h t])
+                       :labels ["h" "t"]
+                       :type "pie"}]
+               :layout {:height 300 :width 600}
+               :config {:displayModeBar false
+                        :displayLogo false}})
+
+^{::clerk/visibility {:result :show}}
+(clerk/plotly {:data [{:y (->> (frequencies (mapv :state (r.buf/read-contents)))
+                               sort
+                               (mapv second))
+                       :x (mapv inc (range 6))
+                       :type "bar"}]
+               :layout {:height 600 :width 600}
+               :config {:displayModeBar false
+                        :displayLogo false}})
+
 
 (def states-map
   {:s0 {:h :s1 :t :s2}
@@ -44,6 +78,8 @@
 
 (comment
 
+  (clerk/serve! {:watch-paths ["test/example"]})
+
   (def result
     (r/run-model global #{next* eita}
                  {:workers 1
@@ -63,6 +99,7 @@
   ;; - [x] Find a way to send a STOP command for simulate/generate flags
   ;; - [x] Send batches of 100 instead of only 1
   ;; - [ ] Generate real-time charts with Clerk
+  ;;   - [ ] Use plotly
   ;; - [ ] Add spec for EWD998
   ;; - [ ] Add implicit `do` to helper macros
   ;; - [ ] Maybe add -noTE when running simulate/generate?
