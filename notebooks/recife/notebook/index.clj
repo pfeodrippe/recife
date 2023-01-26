235257cde67b164d5de85abb9d93d69cbfc4b146
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2023-01-25 20:57:41 -0500

    Add docs

---
+++ notebooks/recife/notebook/index.clj
@@ -0,0 +1,66 @@
+^{:nextjournal.clerk/visibility {:code :hide}}
+(ns recife.notebook.index
+  {:nextjournal.clerk/no-cache true}
+  (:require
+   [nextjournal.clerk :as clerk]
+   [com.pfeodrippe.tooling.clerk :as tool.clerk]))
+
+{::clerk/visibility {:code :hide :result :hide}}
+
+(tool.clerk/add-path-info!
+ {:doc/reasoning 'recife.notebook.reasoning
+  :doc/guide-layout 'recife.notebook.guide-layout
+  :doc/non-determinism 'recife.notebook.non-determinism})
+
+{::clerk/visibility {:code :hide :result :show}}
+
+;; ◊page-name[{:subtitle "A clojure model checker"}]{Recife Guide}
+
+(tool.clerk/view-index
+ [{:title "Intro"
+   :pages [:doc/reasoning]}
+  {:title "Concepts"
+   :pages [:doc/non-determinism]}
+  {:title "Appendix"
+   :pages [:doc/guide-layout]}])
+
+{::clerk/visibility {:code :hide :result :hide}}
+
+(defn build!
+  ([]
+   (build! {}))
+  ([{:keys [browse]
+     :or {browse true}}]
+   (let [exec! #(clerk/build! {:paths ["notebooks/**"]
+                               :index "notebooks/recife/notebook/index.clj"
+                               :bundle true
+                               :browse %})]
+     (when-not browse (exec! false))
+     (exec! browse))))
+
+(comment
+
+  (clerk/serve! {:watch-paths ["notebooks"]})
+
+  (build!)
+
+  ;; TODO:
+  ;; - [ ] Use `dev-tooling`
+  ;; - [ ] Serve thing using GH pages
+  ;; - [ ] proc
+  ;; - [ ] reasoning
+  ;; - [ ] invariant
+  ;; - [ ] temporal property
+  ;; - [ ] action property
+  ;; - [ ] constraint
+  ;; - [ ] action constraint
+  ;; - [ ] tutorial
+  ;; - [ ] design
+  ;;   - [ ] implementation details
+  ;; - [ ] gather data for statistics
+  ;; - [ ] generate
+  ;; - [ ] simulation
+  ;; - [ ] model check
+  ;; - [ ] non determinism
+
+  ())
---
+++ notebooks/recife/notebook/reasoning.clj
@@ -0,0 +1,46 @@
+^{:nextjournal.clerk/visibility {:code :hide}}
+(ns recife.notebook.reasoning
+  {:clerk/name "reasoning"})
+
+;; ◊page-name{🤔 reasoning}
+
+;; Well, in 2020, I've started thinkering with a language called
+;; ◊link{https://lamport.azurewebsites.net/tla/tla.html}{TLA+},
+;; according to their website, it's a "high-level language for
+;; modeling programs and systems". Bold words, right?
+
+;; But it's really different. Not in the Clojure way, it's not the
+;; same kind of disruption that we felt when we came from languages
+;; like Java, Ruby or Python. This time the gap is even bigger, TLA+
+;; is not a language that you use to build systems, but it's a
+;; language you use for thinking about these systems, it's a
+;; specification. You will find concepts like
+;; ◊xref{:doc/non-determinism}, ◊xref{:doc/temporal-properties} or
+;; traces that you don't usually find in our usual day-to-day jobs.
+
+;; While TLA+ is the language, there is also TLC, which is the system
+;; that makes TLA+ waste cycles in our computers. And, fortunately,
+;; TLC is a Java program (would it work with the REPL?)! I've started
+;; to become more interested on hacking it than doing proper
+;; specifications with the language itself, I have even done some
+;; minor contributions to the
+;; ◊link{https://github.com/tlaplus/tlaplus}{TLA+ project}.
+
+;; There is more to it, but to not take much time here, after making
+;; very ugly things to make TLC work with Clojure (in summary, we
+;; can't run a spec in the same JVM because TLC has lots of static
+;; states, so I had to workaround it by making Recife transparently
+;; start a new JVM when you execute ◊code{r/run-model}), Recife was
+;; born.
+
+;; The main purpose in building Recife is to put this kind of tooling
+;; in more hands, but, instead of having to learn TLA+, people can now
+;; use the old Clojure that they know (it's not transpiled
+;; (◊em{mostly}), it's really CLJ in the JVM!). I don't want the fun
+;; to be all mine, time to share it.
+
+;; ----------------------------------------------------------------
+
+;; I recommend you check the work of
+;; ◊link{https://www.hillelwayne.com}{Hillel Wayne}, he was the one
+;; that introduced me (and others) to this world.
