3810e6d726f2bfab4520fe9d3d5adf0d406868ea
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2023-01-25 20:57:41 -0500

    Add docs

---
+++ notebooks/recife/notebook/guide_layout.clj
@@ -0,0 +1,15 @@
+^{:nextjournal.clerk/visibility {:code :hide}}
+(ns recife.notebook.guide-layout
+  {:clerk/name "guide layout"})
+
+;; ◊page-name{guide layout}
+
+;; It's a rip-off of the beautiful
+;; ◊link{https://practicaltypography.com}. We just happen to use
+;; ◊link{https://github.com/nextjournal/clerk}{Clerk} to build this
+;; doc.
+
+;; Which means that everything you read here come from simple clojure
+;; namespaces (including
+;; ◊link{https://github.com/pfeodrippe/recife/blob/master/notebooks/recife/notebook/guide_layout.clj}{this
+;; file}).
---
+++ notebooks/recife/notebook/non_determinism.clj
@@ -0,0 +1,7 @@
+^{:nextjournal.clerk/visibility {:code :hide}}
+(ns recife.notebook.non-determinism
+  {:clerk/name "non-determinism"})
+
+;; ◊page-name[{:subtitle "randomness child"}]{non-determinism}
+
+;; TBD
