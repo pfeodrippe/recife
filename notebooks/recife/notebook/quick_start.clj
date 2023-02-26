6f53e9d11c238b3154e9b359b8f507ff6a9d51f2
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2023-02-25 22:03:12 -0500

    complete quick start

--- notebooks/recife/notebook/quick_start.clj
+++ notebooks/recife/notebook/quick_start.clj
@@ -1,17 +1,19 @@
-^{:nextjournal.clerk/visibility {:code :hide}}
+;; ◊page-name[{:subtitle "Gimme some code"}]{quick start}
+
+;; Run the spec below and see a temporal property violation.
+
 (ns recife.notebook.quick-start
-  {:clerk/name "quick start"}
+  {:nextjournal.clerk/visibility {:result :hide}
+   :clerk/name "quick start"}
   (:require
    [recife.clerk :as rc]
    [recife.core :as r]
    [recife.helpers :as rh]))
 
-;; ◊page-name[{:subtitle "Gimme some code"}]{quick start}
-
 (def global
   {::paid? (r/one-of #{true false})})
 
-(r/defproc ^:fai activate {:local {:pc ::try-activation}}
+(r/defproc ^:fair activate {:local {:pc ::try-activation}}
   {::try-activation
    (fn [{::keys [paid?] :as db}]
      (if paid?
@@ -22,17 +24,12 @@
            (assoc ::status :requires-payment)
            (r/goto ::pay))))
 
-   #_[::pay
+   [::pay
     {:fail? #{true false}}]
-   ::pay
-   (fn [{:keys [fail? oops] :as db}]
-     (when oops
-       (println (keyword (str "___" fail?))))
-     (if #_fail? false
-         (r/done db)
-         (-> db
-             (assoc ::status :activated)
-             r/done)))})
+   (fn [{:keys [fail?] :as db}]
+     (-> db
+         (assoc ::paid? (not fail?))
+         (r/goto ::try-activation)))})
 
 (rh/defproperty will-eventualy-activate
   [{::keys [status]}]
@@ -40,40 +37,10 @@
    (rh/always
     (= status :activated))))
 
-(rh/deffairness pay-fairness
-  [{:keys [::r/procs] :as db}]
-  (rh/fair
-   (rh/and*
-    #_(do (println :>>>procs (:pc (::activate procs)))
-        (not= (:pc (::activate procs)) ::r/done))
-    (rh/for-some [fail? #{true false}]
-      (rh/call activate
-        (assoc db ::r/extra-args {:fail? fail?
-                                  :oops true}))))))
+^{:nextjournal.clerk/visibility {:result :show :code :hide}
+  ::rc/id ::quick}
+(rc/run-model global #{activate will-eventualy-activate})
 
-(comment
-
-  @(r/run-model global
-                #{activate will-eventualy-activate #_pay-fairness}
-                {:trace-example true
-                 :debug true})
-
-  ;; TODO:
-  ;; - [x] Make pay-fairness work
-  ;; - [x] Fix helper variables when using rh/deffairness
-  ;; - [ ] Make meta fair take into consideration the non-deterministic params
-  ;; - [ ] Always go to ::try-activation from ::pay
-
-  ())
-;; graviePayActivationStatus\ =\
-
-;; requires-payment
-;; activated
-
-;; . -> activated if user has already paid
-;; . -> requires-payment if user didn't pay yet
-;; requires-payment -> activated after payment
-
-;; User may initiate paid or not
-
-;; Making a payment requires talking to an unreliable vendor
+;; Ok, this is too much, if you want to go deeper and slower, there is
+;; a series of articles about Recife, starting with
+;; ◊xref{:doc/slow-start}.
