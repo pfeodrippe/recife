d842a05f01ab93518faa10ab614df3e8f8270d53
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2023-05-08 23:49:13 -0400

    finish logic and provenance

--- notebooks/recife/notebook/logic_and_provenance.clj
+++ notebooks/recife/notebook/logic_and_provenance.clj
@@ -1,7 +1,7 @@
 ;; ◊page-name[{:subtitle ""}]{logic and provenance}
 
 ;; We are going to see here Recife being used for ◊em{logic programming}
-;; and for ◊em{provenance}. The first is a poor emulation of the core.logic
+;; and for ◊em{provenance}. The first is a (poor) emulation of the core.logic
 ;; capabilities, the second one consists in searching for a possible state.
 
 (ns recife.notebook.logic-and-provenance
@@ -70,9 +70,9 @@
   {::desired-amount 14
    ::denoms (->> #{1 2 5 10}
                  ;; We will limit the number of coins, min of 0 and
-                 ;; max of 19 for each one.  ◊code{r/one-of} says to
-                 ;; Recife that we would like that the engine test
-                 ;; each of these possibilities as the initial state.
+                 ;; max of 19 for each type. `r/one-of` says to Recife
+                 ;; that we would like the engine to test each one of
+                 ;; the possibilities as the initial state.
                  (mapv (fn [value]
                          [value (r/one-of (range 20))]))
                  (into {}))})
@@ -81,7 +81,9 @@
 ;; similar to ◊code{rh/definvariant}, but instead of checking that
 ;; every value should ◊em{not} be this value, you are saying that you
 ;; would like to find an example that satisfies this check (basically,
-;; one behaves as the complement of the other).
+;; one behaves as the complement of the other), and, if that's the case
+;; a violation will occur (violating the ◊em{non}-occuring, which is what
+;; we want).
 (rh/defchecker check-amount
   [{::keys [desired-amount denoms]}]
   (= desired-amount
@@ -92,8 +94,8 @@
              0
              denoms)))
 
-;; And that should be it, let's now run the model. Note that we are using
-;; the flag ◊code{:continue} here, this means that the engine won't stop
+;; Let's now run the model. Note below that we are using
+;; the flag ◊code{:continue}, this means that the engine won't stop
 ;; in the first violation found (and satisfying the checker is a violation for
 ;; the engine!), it will find as many violations as there are (the downsde of
 ;; using this mode is that temporal properties are not checked, but we are
@@ -103,16 +105,17 @@
   ::rc/block true}
 (rc/run-model den-global #{check-amount} {:continue true})
 
-;; We have a key that we haven't see before, ◊code{:violations}, this
-;; key only appears in the continue mode when there are violations.
-;; The "violations", in our case, are the traces that satisfy the checker,
-;; which is asking which coins sums amount to 14 cents. Each violation only
-;; has the initial state as there is no action to be acted on, but we will
-;; see later in the Provenance section how we can use this for more complex
+;; In the result, we have a key that we haven't see before,
+;; ◊code{:violations}, this key only appears in the continue mode when
+;; there are violations.  The "violations", in our case, are the
+;; traces that satisfy the checker, which is asking which coins
+;; quantities sum to 14 cents. Each violation only has the initial
+;; state as there is no action to be acted on, but we will see later
+;; in the Provenance section how we can use this for more complex
 ;; logic.
 
-;; And yes, these are exactly the same as the one found by the core.logic
-;; example.
+;; And yes, these are exactly the same as the ones found by the
+;; core.logic example.
 ^{:nextjournal.clerk/visibility {:result :show :code :show}}
 (= (->> (r/get-result)
         :violations
@@ -120,7 +123,7 @@
         set)
    (set change-result))
 
-;; Of course, Recife wasn't created for this and it will be slower than
+;; Recife wasn't created for this and it will be slower than
 ;; core.logic, but at least you don't need to learn an entirely different
 ;; language, you can use the normal Clojure you already know.
 
@@ -130,13 +133,137 @@
 ;; at some state. Given that I have these actions, is it possible to reach
 ;; this or that state?
 
-;;
+;; We will use an example derived from a system I had the opportunity
+;; to work with. In the system, we can create employers, parent
+;; employers, upload and insert invoices (these are two separate
+;; actions). Let's do it.
 
-(comment
+;; ◊code{::id} is used as an counter, generating unique ids for the
+;; employers and invoices.
+(def global
+  {::id 0})
 
-  @(r/run-model den-global #{check-amount} {:continue true
-                                            #_ #_:run-local true})
+(def available-employers
+  "Helper function to expose the available employers."
+  (comp keys ::employers))
 
-  (r/get-result)
+(r/defproc add-employer
+  (fn [{::keys [id] :as db}]
+    (-> db
+        (update ::id inc)
+        ;; Create a new employer with a unique ID.
+        ;; Note that `::employers` does not exist in `global`,
+        ;; meaning that we can create (or remove) state as much
+        ;; as we need.
+        (update ::employers assoc
+                id {:id id}))))
 
-  ())
+;; An employer can have an parent, but not a grandparent.
+(r/defproc add-parent-employer
+  {[::set-parent-employer
+    ;; Using a vector as key for the proc map means that we will
+    ;; want things to be non-deterministic (with all paths tested
+    ;; by Recife). E.g. we will randomly choose one parent and one
+    ;; child, and they will be available as the `:parent` and `:child`
+    ;; keys respectively in the associated handler.
+    {:parent available-employers
+     :child available-employers}]
+   (fn [{::keys [employers] :keys [parent child] :as db}]
+     ;; If any of the conditions are not met, then do nothing.
+     (when (and (not= parent child)
+                ;; No grandparents.
+                (not (:parent (get employers child)))
+                (not (:parent (get employers parent)))
+                ;; Also we cannot have recursive parents.
+                (not (contains? (set (mapv :parent (vals employers)))
+                                child)))
+       (-> db
+           (update-in [::employers child] assoc
+                      :parent parent))))})
+
+;; We can also upload an invoice as well as adding it to the system.
+;; For context, they are different actions as the first one is uploaded
+;; manually and the second one is inserted into our DB, but we care
+;; about it in a high-level fashion, we don't really model these details
+;; if we don't need.
+(r/defproc invoice {:local {:pc ::admin-uploads-invoice}}
+  {[::admin-uploads-invoice
+    {:employer available-employers}]
+   (fn [{::keys [id] :keys [employer] :as db}]
+     (when employer
+       (-> db
+           ;; As `:choosen-employer` is an unamespaced keyword,
+           ;; it will be a local variable for the process, only
+           ;; available for `invoice`.
+           (assoc :choosen-employer employer)
+           (update ::id inc)
+           (update-in [::employers employer :invoices-uploaded]
+                      conj id)
+           ;; `r/goto` sends the proc to another step.
+           (r/goto ::system-inserts-invoice))))
+
+   ::system-inserts-invoice
+   (fn [{::keys [employers] :keys [choosen-employer] :as db}]
+     (-> db
+         (update-in [::employers choosen-employer :invoices]
+                    concat
+                    (get-in employers [choosen-employer :invoices-uploaded]))
+         (assoc-in [::employers choosen-employer :invoices-uploaded] [])
+         ;; Back to the initial step.
+         (r/goto ::admin-uploads-invoice)))})
+
+;; In common with the logic programming section, we want to know how
+;; we are able to have an employer with at least one invoice. We
+;; should have the entire trace available as a violation, learning how
+;; our system behaves.
+(rh/defchecker check
+  [{::keys [employers]}]
+  (some (comp seq :invoices)
+        (vals employers)))
+
+;; Add some invariants so we can check that the system is working
+;; correctly.
+(rh/definvariant no-grandparent
+  [{::keys [employers]}]
+  (not (some #(-> (:parent %) employers :parent)
+             (vals employers))))
+
+(rh/definvariant no-cyclical-parent
+  [{::keys [employers]}]
+  (not (some #(= (-> (:parent %) employers :parent)
+                 (:id %))
+             (vals employers))))
+
+;; Also a constraint so we don't add infinite employers, it would be
+;; useless.
+(rh/defconstraint constraint
+  [{::keys [employers]}]
+  (<= (count employers) 3))
+
+^{:nextjournal.clerk/visibility {:result :show :code :hide}
+  ::rc/id ::provenance
+  ::rc/block true}
+(rc/run-model global #{add-employer add-parent-employer invoice
+                       check
+                       no-grandparent no-cyclical-parent
+                       constraint})
+
+;; Nice! We have been able to find an example of what
+;; we wanted, see the table below for more details.
+
+^{:nextjournal.clerk/visibility {:result :show :code :hide}}
+(clerk/table
+ (->> (r/get-result)
+      :trace
+      (mapv last)
+      (mapv #(select-keys % [::id ::employers :recife/metadata]))
+      (mapv #(-> %
+                 (update ::employers clerk/code)
+                 (update :recife/metadata clerk/code)))))
+
+;; You can see the behavior that leaded to each step along with the values,
+;; so you can debug not only static data like we did in our logic programming
+;; section, but also dynamic actions, being able to debug or learn about how
+;; some good thing or bad thing happened.
+
+;; That's it for today, see ya o/
